cd511e5d1a0c304305bf411055cd56396017f978
tslohac <hackandcoding@gmail.com>
2025-08-18 03:20:43 +0530

    containorised the player object for now

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     p1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -24,18 +27,22 @@ def main():
 
     game_time = pygame.time.Clock()
     dt = 0
+
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-
-        p1.update(dt)
-        p1.draw(screen)
+        dt = game_time.tick(60)/1000
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         # game_time.tick(60)
-        dt = game_time.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
