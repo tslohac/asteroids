8651240b596f91ff050b661d6da26503d1cc32eb
tslohac <hackandcoding@gmail.com>
2025-08-17 18:21:55 +0530

    fixed screen fill and added game quit event

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
-        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
 
         pygame.display.flip()
 
